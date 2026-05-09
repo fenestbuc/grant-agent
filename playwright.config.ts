8e9ac3b8dc10aa441fc933f2a55b00efcaa24087
Vaibhav Sharma <vaibhavsharma7403@gmail.com>
2026-05-10 05:03:51 +0530

    chore: setup vercel deployment and playwright webserver

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,5 +17,11 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-  ]
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  }
 });
